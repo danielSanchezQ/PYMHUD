a261d367acd6f87c47e8d8b7efa2f33c25831b69
unknown <Dani@Dani-PC.(none)>
2012-05-08 19:51:57 +0200

    first tests

--- src/pymhud.py
+++ src/pymhud.py
@@ -2,3 +2,13 @@
  BY DANIEL SANCHEZ QUIROS"""
 
 import pymel.core as pm
+
+
+def custom_hud(names):
+    """TESTING"""
+    pm.headsUpDisplay("Current_Camera", section = 1, block = 2, label= "position", labelFontSize = "small")
+
+"""MORE TESTING"""	
+cameraHUD = custom_hud("names")	
+pm.headsUpDisplay("Current_Camera", rem = True)
+pm.headsUpDisplay(cameraHUD[1], rid = True)
