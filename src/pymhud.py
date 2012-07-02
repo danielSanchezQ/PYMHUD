ae945df7f9f49814b790a3e4eb05acbda338c367
unknown <Dani@Dani-PC.(none)>
2012-07-02 13:27:23 +0200

    UI updates

--- src/pymhud.py
+++ src/pymhud.py
@@ -12,14 +12,13 @@ class Custom_hud():
         self.AnimatorName = "Daniel"
         self.Date = pm.date(format = "DD/MM/YYYY")
         self.SceneName = "prueba"
-        #self.window = pm.window(menuBar = False, widthHeight = [300, 200])
         self.activesHuds = []
         self.__init_window__()
     
     def createHud(self):
-        self.activesHuds.append(int(pm.headsUpDisplay("AnimatorName", label= self.AnimatorName, labelFontSize = "small", section = 5, block = 1)))
+        self.activesHuds.append(int(pm.headsUpDisplay("AnimatorName", label = self.AnimatorName, labelFontSize = "small", section = 5, block = 1)))
         self.activesHuds.append(int(pm.headsUpDisplay("Date", label= self.Date, labelFontSize = "small", section = 6,block = 1)))
-        self.activesHuds.append(int(pm.headsUpDisplay("SceneName", label= self.SceneName, labelFontSize = "small", section = 7,block = 1)))
+        self.activesHuds.append(int(pm.headsUpDisplay("SceneName", label = self.SceneName, labelFontSize = "small", section = 7,block = 1)))
         self.frameHud()
         
     def frameHud(self):
@@ -34,14 +33,21 @@ class Custom_hud():
         pm.deleteUI(self.window, window = True)
         
     def __init_window__(self):
-        self.window = pm.window(menuBar = False, widthHeight = [300, 200])
+        self.window = pm.window(menuBar = False, widthHeight = [200, 100], title = "PYMHUD by Daniel Sánchez Quirós")
         self.columna = pm.columnLayout(adjustableColumn = True)
         self.row1 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
-        pm.text("Name:", parent = self.row1)
+        pm.text("Name:",al = "left", parent = self.row1)
         self.nameQ = pm.textField(parent = self.row1)
-        pm.button(parent = self.row1)
-        self.row2 =pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
-        pm.text("Scene Name")
+        pm.checkBox(label = "", parent = self.row1)
+        self.row2 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
+        pm.text("Scene Name:",al = "left")
         self.sceneNameQ = pm.textField(parent = self.row2)
-        pm.button(parent = self.row2)
+        pm.checkBox(label = "", parent = self.row2)
+        self.row3 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
+        pm.text("Date:",al = "left")
+        pm.checkBox(label = "", parent = self.row3)
+        self.row4 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
+        pm.text("Frame Counter:",al = "left")
+        pm.checkBox(label = "", parent = self.row4)
         pm.showWindow(self.window)
+        
