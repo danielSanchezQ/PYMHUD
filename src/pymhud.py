5842fab9c564e1dfa80e42138de6cb0f8716f38a
unknown <Dani@Dani-PC.(none)>
2012-07-02 16:09:24 +0200

    UI checkbox working

--- src/pymhud.py
+++ src/pymhud.py
@@ -3,6 +3,7 @@ CUSTOM HEADS UP DISPLAY PYMEL MODULE FOR MAYA
 DONE BY DANIEL SANCHEZ QUIROS, 3danimanimal@gmail.com
 """
 import pymel.core as pm
+from pymel.core.windows import Callback
 
 class Custom_hud():
     def __init__(self):
@@ -35,19 +36,25 @@ class Custom_hud():
     def __init_window__(self):
         self.window = pm.window(menuBar = False, widthHeight = [200, 100], title = "PYMHUD by Daniel Sánchez Quirós")
         self.columna = pm.columnLayout(adjustableColumn = True)
+        #Name row
         self.row1 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
         pm.text("Name:",al = "left", parent = self.row1)
         self.nameQ = pm.textField(parent = self.row1)
         pm.checkBox(label = "", parent = self.row1)
+        #Scene row
         self.row2 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
         pm.text("Scene Name:",al = "left")
         self.sceneNameQ = pm.textField(parent = self.row2)
         pm.checkBox(label = "", parent = self.row2)
+        #Date row
         self.row3 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
         pm.text("Date:",al = "left")
         pm.checkBox(label = "", parent = self.row3)
+        #rame counter row
         self.row4 = pm.rowLayout(numberOfColumns=3, adjustableColumn = True, parent = self.columna)
         pm.text("Frame Counter:",al = "left")
-        pm.checkBox(label = "", parent = self.row4)
+        pm.checkBox(label = "", parent = self.row4,ofc = Callback(self.frameHud), onCommand = Callback(self.frameHud))
         pm.showWindow(self.window)
         
+    def __del__(self):
+        pass
